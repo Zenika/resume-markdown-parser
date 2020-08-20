9c80b1837559bd1143d7090dbf737b3c2940acff
Ked57 <kedketh@gmail.com>
2020-08-20 14:29:43 +0200

    :sparkles: Add output to template html file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { readFileSync, writeFileSync } from "fs";
 import { program } from "commander";
 import commonmark from "commonmark";
 
@@ -9,7 +10,7 @@ const preProcessInput = (input: string) =>
     .replace(/--expertise-start.*--expertise-end/gm, "")
     .replace(/--section-start/gm, `<div class="section">`)
     .replace(/--section-end/gm, `</div>`)
-    .replace(/--break-page/gm, `<div class="break-page" />`)
+    .replace(/--break-page/gm, `<div class="break-page"></div>`)
     .replace(/\\n/gm, "\n");
 
 const handleInput = (input: string) => {
@@ -19,6 +20,11 @@ const handleInput = (input: string) => {
   //   console.log("parsed", parsed);
   const rendered = writer.render(parsed);
   console.log("rendered", rendered);
+  const template = readFileSync("template.html").toString();
+  console.log("opened file", template);
+  const result = template.replace("#content", rendered);
+  console.log("result", result);
+  writeFileSync("output.html", result, { encoding: "utf-8" });
 };
 
 program.version("0.0.1").arguments("<input>").action(handleInput);
