ab3d82ea25c37b6bec06708c6439a936636331c6
Ked57 <kedketh@gmail.com>
2020-08-21 13:50:06 +0200

    :wastebasket: Clean up the code

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import commonmark from "commonmark";
 const parser = new commonmark.Parser();
 const writer = new commonmark.HtmlRenderer();
 
+const template = readFileSync("template.html").toString();
+
 const preProcessInput = (input: string) =>
   input
     .replace(/--expertise-start.*--expertise-end/gm, "")
@@ -15,16 +17,11 @@ const preProcessInput = (input: string) =>
 
 const handleInput = (input: string) => {
   const preProcessedInput = preProcessInput(input);
-  //   console.log("preprocessed", preProcessedInput);
   const parsed = parser.parse(preProcessedInput);
-  //   console.log("parsed", parsed);
   const rendered = writer.render(parsed);
-  console.log("rendered", rendered);
-  const template = readFileSync("template.html").toString();
-  console.log("opened file", template);
   const result = template.replace("#content", rendered);
-  console.log("result", result);
   writeFileSync("output.html", result, { encoding: "utf-8" });
+  console.log("resume processed succesfully")
 };
 
 program.version("0.0.1").arguments("<input>").action(handleInput);
